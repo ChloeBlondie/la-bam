b7c4dbc44c2c51d3007fcafef57e42bfebb355da
Chloe Blondie <chloe.bloondie@gmail.com>
2019-10-29 22:10:14 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { 
+  sonorityOptions,
+  // sizeOptions,
+  // originalityOptions,
+  // languageOptions
+} from './constants.js';
+import map from 'lodash/map';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <div class="container"> */}
+        <form>
+          <div class="form-group">
+
+            <div class="form-group">
+              <label for="sonority">Sonorité</label>
+              <select class="form-control" id="sonority">
+                {map(sonorityOptions, option => <option key={option.value}>{option.label}</option>)}
+              </select>
+              <small id="sonorityHelp" class="form-text text-muted">Choisis une sonorité</small>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Générer</button>
+          </div>
+
+        </form>
       </header>
     </div>
   );
---
+++ src/constants.js
@@ -0,0 +1,8 @@
+export const sonorityOptions = [
+    {value: 0, label:'Doux'},
+    {value: 1, label:'Normal'},
+    {value: 2, label:'Dur'}
+];
+export const sizeOptions = ['Court', 'Moyen', 'Long', 'Très long'];
+export const originalityOptions = ['Courant', 'Original', 'Très original', 'Déglingué'];
+export const languageOptions = ['Francais', 'Anglais', 'Allemand', 'Espagnol', 'Italien'];
